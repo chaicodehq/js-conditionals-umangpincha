0024cc6126b66d53ce8610a3a59e64921f2f018c
umangpincha <umangpincha@gmail.com>
2026-02-08 14:25:18 +0530

    Fix question 8th

--- src/08-tax-calculator.js
+++ src/08-tax-calculator.js
@@ -27,4 +27,16 @@
  */
 export function calculateTax(income) {
   // Your code here
+  if(income === 0 || income < 0){
+    return 0;
+  }
+  else if(income>0 && income<=10000){
+    return 0;
+  }else if(income>=10001 && income<=30000){
+    return((income - 10000)) * 0.1;
+  }else if(income>=30001 && income<=70000){
+    return (2000 + ((income - 30000) * 0.2));
+  }else{
+    return (2000 + 8000 + ((income - 70000)*0.3))
+  }
 }
