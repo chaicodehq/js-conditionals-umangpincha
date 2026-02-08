30b802a3c298395c34539fa9bafd13167068eeeb
umangpincha <umangpincha@gmail.com>
2026-02-08 12:20:25 +0530

    Fix question 3rd

--- src/03-grade-calculator.js
+++ src/03-grade-calculator.js
@@ -33,15 +33,17 @@ export function calculateGrade(score, hasExtraCredit) {
     if(score > 100){
       score = 100;
     }
-  }else if(score >= 90 || score <= 100){
+  }
+  
+  if(score >= 90 && score <= 100){
     return "A";
-  }else if(score >=80 || score <=89){
+  }else if(score >=80 && score <=89){
     return "B";
-  }else if(score >=70 || score <= 79){
+  }else if(score >=70 && score <= 79){
     return "C";
-  }else if(score >=60 || score <= 69){
+  }else if(score >=60 && score <= 69){
     return "D";
-  }else if(score >=0 || score <= 59){
+  }else if(score >=0 && score <= 59){
     return "F";
   }
 
